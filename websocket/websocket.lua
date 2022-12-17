2832e155b031ab66b306e568dbbe3f2841e6bc17
notweuz <51354026+notweuz@users.noreply.github.com>
2022-12-17 22:40:35 +0400

    Add files via upload

--- websocket/websocket.lua
+++ websocket/websocket.lua
@@ -1,23 +1,16 @@
-local Websocket_Manager = {}
-
-self = Websocket_Manager
-
-Websocket_Manager = {
-    Websocket = nil,
+local Websocket_Manager = {
     ['Connect'] = function()
         if Krnl then
             if pcall(function() Krnl.Websocket.connect("ws://130.61.108.155:8888/") end) then
-                self.websocket = Krnl.Websocket.connect("ws://130.61.108.155:8888/")
-                return self.Websocket
+                return Krnl.Websocket.connect("ws://130.61.108.155:8888/")
             else
-                return false
+                return nil
             end
         elseif syn then
             if pcall(function()syn.websocket.connect("ws://130.61.108.155:8888/") end) then
-                self.websocket = syn.websocket.connect("ws://130.61.108.155:8888/")
-                return self.Websocket
+                return syn.websocket.connect("ws://130.61.108.155:8888/")
             else
-                return false
+                return nil
             end
         end
     end
